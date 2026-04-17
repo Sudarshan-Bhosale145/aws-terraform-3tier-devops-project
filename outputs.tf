4e32179e60f07254a83848066c4166b7adf663e4
Sudarshan-Bhosale145 <bhosalesudarshan2003@gmail.com>
2026-04-17 14:01:52 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "web_public_ip"   { value = module.web_ec2.public_ip }
+output "app_private_ip"  { value = module.app_ec2.private_ip }
+output "app_public_ip"   { value = module.app_ec2.public_ip }
+output "rds_endpoint"    { value = module.rds.db_endpoint }
