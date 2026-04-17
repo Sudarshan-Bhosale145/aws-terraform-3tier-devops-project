dbde12410f552a3fce79cd1bc2f47f0ed1853df1
Sudarshan-Bhosale145 <bhosalesudarshan2003@gmail.com>
2026-04-17 13:55:20 +0530

    Delete init.sql

--- init.sql
+++
@@ -1,9 +0,0 @@
-USE mydb;
-
-CREATE TABLE IF NOT EXISTS registrations (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    email VARCHAR(100),
-    phone VARCHAR(20),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
